c90464049e86368dffea771ad27c61846ffc66a1
Martin <martin.lippl@pueo.de>
2020-04-18 15:06:47 +0200

    * Added option to write image of top-down projection of lidar data to a file

--- src/camFusion_Student.cpp
+++ src/camFusion_Student.cpp
@@ -108,7 +108,8 @@ void show3DObjects(std::vector<BoundingBox> &boundingBoxes, cv::Size worldSize, 
         sprintf(str1, "id=%d, #pts=%d", it1->boxID, (int)it1->lidarPoints.size());
         putText(topviewImg, str1, cv::Point2f(left-250, bottom+50), cv::FONT_ITALIC, 2, currColor);
         sprintf(str2, "xmin=%2.2f m, yw=%2.2f m", xwmin, ywmax-ywmin);
-        putText(topviewImg, str2, cv::Point2f(left-250, bottom+125), cv::FONT_ITALIC, 2, currColor);  
+        putText(topviewImg, str2, cv::Point2f(left-250, bottom+125), cv::FONT_ITALIC, 2, currColor); 
+        //cv::imwrite("./lidar.png", topviewImg);
     }
 
     // plot distance markers
